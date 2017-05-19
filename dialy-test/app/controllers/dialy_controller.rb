3dcd4fd869896d0547fa0fe84a922819620d0439
shokei <apple.4869.s@gmail.com>
2017-05-20 04:40:34 +0900

    ・railsに必要そうなファイルを設定 ・headerをコーディング ・MySQLはまだの方向で

---
+++ dialy-test/app/controllers/dialy_controller.rb
@@ -0,0 +1,24 @@
+class DialyController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def mypage
+  end
+
+  def destory
+  end
+
+  def login
+  end
+
+  def logout
+  end
+
+end
